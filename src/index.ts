a3b57b92f0cd7d7eb12b3a8ab085d140bf8c74e3
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-01-24 16:39:00 +0900

    implement function anotation

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import World from './world'
 const root = document.getElementById('root')
 const world = new World('Hello World!')
 world.sayHello(root)
+
+// 関数の型定義
+import { logMessage } from './function/basic'
+
+logMessage('hello.')
